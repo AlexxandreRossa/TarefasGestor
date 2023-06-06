15a6567891afaa8d3da734fece3595f5f7570b7e
Alexandre Rossa <alexxandrerossa@gmail.com>
2023-06-06 04:44:19 +0100

    Reformulação da data de vencimento na tabela de tarefas

--- app/src/main/java/com/example/tarefas_gestor/TabelaTarefas.kt
+++ app/src/main/java/com/example/tarefas_gestor/TabelaTarefas.kt
@@ -5,14 +5,17 @@ import android.provider.BaseColumns
 
 class TabelaTarefas(db: SQLiteDatabase) : TabelaBD(db, NOME_TABELA) {
     override fun cria() {
-        db.execSQL("CREATE TABLE $NOME_TABELA ($CHAVE_TABELA, $CAMPO_NOME TEXT NOT NULL, $CAMPO_DESCRICAO TEXT, $CAMPO_DATA_VENCIMENTO TEXT, $CAMPO_FK_CATEGORIA INTEGER NOT NULL, FOREIGN KEY ($CAMPO_FK_CATEGORIA) REFERENCES ${TabelaCategorias.NOME_TABELA}(${BaseColumns._ID}) ON DELETE RESTRICT)")
+        db.execSQL("CREATE TABLE $NOME_TABELA ($CHAVE_TABELA, $CAMPO_NOME TEXT NOT NULL, $CAMPO_DESCRICAO TEXT, $CAMPO_DATA_VENCIMENTO INTEGER, $CAMPO_FK_CATEGORIA INTEGER NOT NULL, FOREIGN KEY ($CAMPO_FK_CATEGORIA) REFERENCES ${TabelaCategorias.NOME_TABELA}(${BaseColumns._ID}) ON DELETE RESTRICT)")
     }
 
     companion object {
         const val NOME_TABELA = "tarefas"
+
         const val CAMPO_NOME = "nome"
         const val CAMPO_DESCRICAO = "descricao"
         const val CAMPO_DATA_VENCIMENTO = "data_vencimento"
         const val CAMPO_FK_CATEGORIA = "id_categoria"
+
+        val CAMPOS = arrayOf(BaseColumns._ID, CAMPO_NOME, CAMPO_DESCRICAO, CAMPO_DATA_VENCIMENTO, CAMPO_FK_CATEGORIA)
     }
 }
--- app/src/main/java/com/example/tarefas_gestor/Tarefa.kt
+++ app/src/main/java/com/example/tarefas_gestor/Tarefa.kt
@@ -1,10 +1,14 @@
 package com.example.tarefas_gestor
 
 import android.content.ContentValues
+import android.database.Cursor
+import android.media.audiofx.AudioEffect.Descriptor
+import android.provider.BaseColumns
+import java.util.Calendar
 data class Tarefa(
     var nome: String,
     var descricao: String,
-    var data_vencimento: String,
+    var data_vencimento: Calendar? = null,
     var id_categoria: Long,
     var id: Long = -1
 ) {
@@ -13,9 +17,36 @@ data class Tarefa(
 
         valores.put(TabelaTarefas.CAMPO_NOME, nome)
         valores.put(TabelaTarefas.CAMPO_DESCRICAO, descricao)
-        valores.put(TabelaTarefas.CAMPO_DATA_VENCIMENTO, data_vencimento)
+        valores.put(TabelaTarefas.CAMPO_DATA_VENCIMENTO, data_vencimento?.timeInMillis)
         valores.put(TabelaTarefas.CAMPO_FK_CATEGORIA, id_categoria)
 
         return valores
+    }
+
+    companion object {
+        fun fromCursor(cursor: Cursor) : Tarefa {
+            val posId = cursor.getColumnIndex(BaseColumns._ID)
+            val posNome = cursor.getColumnIndex(TabelaTarefas.CAMPO_NOME)
+            val posDescricao = cursor.getColumnIndex(TabelaTarefas.CAMPO_DESCRICAO)
+            val posDataVencimento = cursor.getColumnIndex(TabelaTarefas.CAMPO_DATA_VENCIMENTO)
+            val posCategoriaFK = cursor.getColumnIndex(TabelaTarefas.CAMPO_FK_CATEGORIA)
+
+            val id = cursor.getLong(posId)
+            val nome = cursor.getString(posNome)
+            val descricao = cursor.getString(posDescricao)
+
+            var dataVencimento: Calendar?
+
+            if (cursor.isNull(posDataVencimento)) {
+                dataVencimento = null
+            } else {
+                dataVencimento = Calendar.getInstance()
+                dataVencimento.timeInMillis = cursor.getLong(posDataVencimento)
+            }
+
+            val categoriaId = cursor.getLong(posCategoriaFK)
+
+            return Tarefa(nome,descricao,dataVencimento,categoriaId,id)
+        }
     }
 }
