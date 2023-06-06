456359c78249971e2e7b310650bf915e3ab0d04a
Alexandre Rossa <alexxandrerossa@gmail.com>
2023-06-06 04:01:59 +0100

    Criar função para converter uma categoria em ContentValues

--- app/src/main/java/com/example/tarefas_gestor/Categoria.kt
+++ app/src/main/java/com/example/tarefas_gestor/Categoria.kt
@@ -1,8 +1,19 @@
 package com.example.tarefas_gestor
 
+import android.content.ContentValues
+
 data class Categoria(
     var nome: String,
     var cor: String,
     var id: Long = -1
 ) {
+
+    fun toContentValues() : ContentValues {
+        val valores = ContentValues()
+
+        valores.put(TabelaCategorias.CAMPO_NOME, nome)
+        valores.put(TabelaCategorias.CAMPO_COR, cor)
+
+        return valores
+    }
 }
--- app/src/main/java/com/example/tarefas_gestor/TabelaCategorias.kt
+++ app/src/main/java/com/example/tarefas_gestor/TabelaCategorias.kt
@@ -3,10 +3,12 @@ package com.example.tarefas_gestor
 import android.database.sqlite.SQLiteDatabase
 class TabelaCategorias(db: SQLiteDatabase) : TabelaBD(db, NOME_TABELA) {
     override fun cria() {
-        db.execSQL("CREATE TABLE $NOME_TABELA ($CHAVE_TABELA, nome TEXT NOT NULL, cor TEXT NOT NULL)")
+        db.execSQL("CREATE TABLE $NOME_TABELA ($CHAVE_TABELA, $CAMPO_NOME TEXT NOT NULL, $CAMPO_COR TEXT NOT NULL)")
     }
 
     companion object {
         const val NOME_TABELA = "categorias"
+        const val CAMPO_NOME = "nome"
+        const val CAMPO_COR = "cor"
     }
 }
