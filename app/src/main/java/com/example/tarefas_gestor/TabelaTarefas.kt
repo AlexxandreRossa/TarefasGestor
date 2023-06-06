8bac688f3bedae978d7c45474a23adfb81c05173
Alexandre Rossa <alexxandrerossa@gmail.com>
2023-06-06 04:08:12 +0100

    Criar função para converter uma tarefa em ContentValues

--- app/src/main/java/com/example/tarefas_gestor/TabelaTarefas.kt
+++ app/src/main/java/com/example/tarefas_gestor/TabelaTarefas.kt
@@ -5,10 +5,14 @@ import android.provider.BaseColumns
 
 class TabelaTarefas(db: SQLiteDatabase) : TabelaBD(db, NOME_TABELA) {
     override fun cria() {
-        db.execSQL("CREATE TABLE $NOME_TABELA ($CHAVE_TABELA, nome TEXT NOT NULL, descrico TEXT,data_vencimento INTEGER NOT NULL, id_categoria INTEGER NOT NULL, FOREIGN KEY (id_categoria) REFERENCES ${TabelaCategorias.NOME_TABELA}(${BaseColumns._ID}) ON DELETE RESTRICT)")
+        db.execSQL("CREATE TABLE $NOME_TABELA ($CHAVE_TABELA, $CAMPO_NOME TEXT NOT NULL, $CAMPO_DESCRICAO TEXT, $CAMPO_DATA_VENCIMENTO TEXT, $CAMPO_FK_CATEGORIA INTEGER NOT NULL, FOREIGN KEY ($CAMPO_FK_CATEGORIA) REFERENCES ${TabelaCategorias.NOME_TABELA}(${BaseColumns._ID}) ON DELETE RESTRICT)")
     }
 
     companion object {
         const val NOME_TABELA = "tarefas"
+        const val CAMPO_NOME = "nome"
+        const val CAMPO_DESCRICAO = "descricao"
+        const val CAMPO_DATA_VENCIMENTO = "data_vencimento"
+        const val CAMPO_FK_CATEGORIA = "id_categoria"
     }
 }
--- app/src/main/java/com/example/tarefas_gestor/Tarefa.kt
+++ app/src/main/java/com/example/tarefas_gestor/Tarefa.kt
@@ -1,5 +1,6 @@
 package com.example.tarefas_gestor
 
+import android.content.ContentValues
 data class Tarefa(
     var nome: String,
     var descricao: Int,
@@ -7,4 +8,14 @@ data class Tarefa(
     var id_categoria: Int,
     var id: Long = -1
 ) {
+    fun toContentValues() : ContentValues {
+        val valores = ContentValues()
+
+        valores.put(TabelaTarefas.CAMPO_NOME, nome)
+        valores.put(TabelaTarefas.CAMPO_DESCRICAO, descricao)
+        valores.put(TabelaTarefas.CAMPO_DATA_VENCIMENTO, data_vencimento)
+        valores.put(TabelaTarefas.CAMPO_FK_CATEGORIA, id_categoria)
+
+        return valores
+    }
 }
