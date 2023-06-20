48e413a6800efbd4ac34f7dcaaf27243a044dcb1
Alexandre Rossa <alexxandrerossa@gmail.com>
2023-06-20 11:59:49 +0100

    Mostrar a tarefa selecionada

--- app/src/main/java/com/example/tarefas_gestor/AdapterTarefas.kt
+++ app/src/main/java/com/example/tarefas_gestor/AdapterTarefas.kt
@@ -3,6 +3,7 @@ package com.example.tarefas_gestor
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import android.database.Cursor
@@ -19,6 +20,13 @@ class AdapterTarefas(val fragment: ListaTarefasFragment) : RecyclerView.Adapter<
         private val textViewNome = contentor.findViewById<TextView>(R.id.textViewNome)
         private val textViewCategoria = contentor.findViewById<TextView>(R.id.textViewCategoria)
 
+        init {
+            contentor.setOnClickListener {
+                viewHolderSeleccionado?.desSeleciona()
+                seleciona()
+            }
+        }
+
 
 
         internal var tarefa: Tarefa? = null
@@ -27,6 +35,19 @@ class AdapterTarefas(val fragment: ListaTarefasFragment) : RecyclerView.Adapter<
                 textViewNome.text = tarefa?.nome ?: ""
                 textViewCategoria.text = tarefa?.id_categoria.toString() ?: ""
             }
+
+
+        fun seleciona() {
+            viewHolderSeleccionado = this
+            itemView.setBackgroundResource(R.color.item_selecionado)
+        }
+
+        fun desSeleciona() {
+            itemView.setBackgroundResource(android.R.color.white)
+        }
+
+        private var viewHolderSeleccionado : ViewHolderTarefa? = null
+
     }
 
     /**
