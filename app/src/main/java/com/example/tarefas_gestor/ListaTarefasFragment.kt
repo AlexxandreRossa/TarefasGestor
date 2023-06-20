a2f21eb40049dcaa22758c21181703c7605f65c2
Alexandre Rossa <alexxandrerossa@gmail.com>
2023-06-20 10:50:45 +0100

    Inicializar o RecyclerView de tarefas (adapter e layout manager)

--- app/src/main/java/com/example/tarefas_gestor/ListaTarefasFragment.kt
+++ app/src/main/java/com/example/tarefas_gestor/ListaTarefasFragment.kt
@@ -5,23 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.tarefas_gestor.databinding.FragmentListaTarefasBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 class ListaTarefasFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private var _binding: FragmentListaTarefasBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -32,23 +29,15 @@ class ListaTarefasFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_lista_tarefas, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val adapterTarefas = AdapterTarefas()
+        binding.recyclerviewtarefas.adapter = adapterTarefas
+        binding.recyclerviewtarefas.layoutManager = LinearLayoutManager(requireContext())
+    }
+
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ListaLivrosFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ListaTarefasFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
     }
 }
