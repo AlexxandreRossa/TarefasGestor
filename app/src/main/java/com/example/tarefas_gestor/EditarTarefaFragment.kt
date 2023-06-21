099b7bb265221ffa3da12877a84de2b83be1b246
Alexandre Rossa <alexxandrerossa@gmail.com>
2023-06-21 16:08:55 +0100

    Guardar tarefa (editar / novo)

--- app/src/main/java/com/example/tarefas_gestor/EditarTarefaFragment.kt
+++ app/src/main/java/com/example/tarefas_gestor/EditarTarefaFragment.kt
@@ -13,7 +13,9 @@ import androidx.loader.app.LoaderManager
 import androidx.loader.content.CursorLoader
 import androidx.loader.content.Loader
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
 import java.text.SimpleDateFormat
+import android.net.Uri
 import java.util.Calendar
 import java.util.Date
 
@@ -61,7 +63,7 @@ class EditarTarefaFragment : Fragment(), LoaderManager.LoaderCallbacks<Cursor> {
             }
         }
 
-        this.livro = livro
+        this.tarefa = tarefa
     }
 
     override fun onDestroyView() {
@@ -118,15 +120,49 @@ class EditarTarefaFragment : Fragment(), LoaderManager.LoaderCallbacks<Cursor> {
     var id_categoria: Long,
     var id: Long = -1
          */
+        val novadataVencimento = Calendar.getInstance()
+        novadataVencimento.set(2023, 6, 10)
 
-        val dataVencimento3 = Calendar.getInstance()
-        dataVencimento3.set(2023, 6, 10)
+        if (tarefa == null) {
+            val livro = Tarefa(
+                nome,
+                ("Fazer os TPC's"),
+                novadataVencimento,
+                categoriaId)
 
-        val tarefa = Tarefa(
-            nome,
-            ("Fazer os TPC's"),
-            dataVencimento3,
-            categoriaId)
+
+            insereTarefa(tarefa)
+
+        } else {
+            val tarefa = tarefa!!
+            tarefa.nome = nome
+            tarefa.descricao = ("Fazer os TPC's")
+            tarefa.data_vencimento = novadataVencimento
+            tarefa.id_categoria = categoriaId
+
+            alteraTarefa(tarefa)
+        }
+    }
+
+    private fun alteraTarefa(tarefa: Tarefa) {
+        val enderecoTarefa = Uri.withAppendedPath(TarefasContentProvider.ENDERECO_TAREFAS, tarefa.id.toString())
+        val tarefasAlteradas = requireActivity().contentResolver.update(enderecoTarefa, tarefa.toContentValues(), null, null)
+
+        if (tarefasAlteradas == 1) {
+            Toast.makeText(requireContext(), R.string.tarefa_guardada_com_sucesso, Toast.LENGTH_LONG).show()
+            voltaListaTarefas()
+        } else {
+            binding.editTextTitulo.error = getString(R.string.erro_guardar_tarefa)
+        }
+    }
+
+    override fun onLoaderReset(loader: Loader<Cursor>) {
+        binding.spinnerCategorias.adapter = null
+    }
+
+    private fun insereTarefa(
+        tarefa: Tarefa
+    ) {
 
         val id = requireActivity().contentResolver.insert(
             TarefasContentProvider.ENDERECO_TAREFAS,
@@ -138,7 +174,12 @@ class EditarTarefaFragment : Fragment(), LoaderManager.LoaderCallbacks<Cursor> {
             return
         }
 
-        Toast.makeText(requireContext(), getString(R.string.tarefa_guardada_com_sucesso), Toast.LENGTH_SHORT).show()
+        Toast.makeText(
+            requireContext(),
+            getString(R.string.tarefa_guardada_com_sucesso),
+            Toast.LENGTH_SHORT
+        ).show()
+
         voltaListaTarefas()
 
     }
@@ -153,63 +194,6 @@ class EditarTarefaFragment : Fragment(), LoaderManager.LoaderCallbacks<Cursor> {
         )
     }
 
-    /**
-     * Called when a previously created loader is being reset, and thus
-     * making its data unavailable.  The application should at this point
-     * remove any references it has to the Loader's data.
-     *
-     *
-     * This will always be called from the process's main thread.
-     *
-     * @param loader The Loader that is being reset.
-     */
-    override fun onLoaderReset(loader: Loader<Cursor>) {
-        binding.spinnerCategorias.adapter = null
-    }
-
-    /**
-     * Called when a previously created loader has finished its load.  Note
-     * that normally an application is *not* allowed to commit fragment
-     * transactions while in this call, since it can happen after an
-     * activity's state is saved.  See [ FragmentManager.openTransaction()][androidx.fragment.app.FragmentManager.beginTransaction] for further discussion on this.
-     *
-     *
-     * This function is guaranteed to be called prior to the release of
-     * the last data that was supplied for this Loader.  At this point
-     * you should remove all use of the old data (since it will be released
-     * soon), but should not do your own release of the data since its Loader
-     * owns it and will take care of that.  The Loader will take care of
-     * management of its data so you don't have to.  In particular:
-     *
-     *
-     *  *
-     *
-     *The Loader will monitor for changes to the data, and report
-     * them to you through new calls here.  You should not monitor the
-     * data yourself.  For example, if the data is a [android.database.Cursor]
-     * and you place it in a [android.widget.CursorAdapter], use
-     * the [android.widget.CursorAdapter.CursorAdapter] constructor *without* passing
-     * in either [android.widget.CursorAdapter.FLAG_AUTO_REQUERY]
-     * or [android.widget.CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER]
-     * (that is, use 0 for the flags argument).  This prevents the CursorAdapter
-     * from doing its own observing of the Cursor, which is not needed since
-     * when a change happens you will get a new Cursor throw another call
-     * here.
-     *  *  The Loader will release the data once it knows the application
-     * is no longer using it.  For example, if the data is
-     * a [android.database.Cursor] from a [android.content.CursorLoader],
-     * you should not call close() on it yourself.  If the Cursor is being placed in a
-     * [android.widget.CursorAdapter], you should use the
-     * [android.widget.CursorAdapter.swapCursor]
-     * method so that the old Cursor is not closed.
-     *
-     *
-     *
-     * This will always be called from the process's main thread.
-     *
-     * @param loader The Loader that has finished.
-     * @param data The data generated by the Loader.
-     */
     override fun onLoadFinished(loader: Loader<Cursor>, data: Cursor?) {
         if (data == null) {
             binding.spinnerCategorias.adapter = null
